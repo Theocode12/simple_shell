cf561fea326a7b7e26888439bc693d20fd36e15d
Yahaya Azeez <azconcept2016@gmail.com>
2022-05-18 07:32:36 +0000

    Add comments

--- main.h
+++ main.h
@@ -22,12 +22,19 @@ typedef struct builtin_fun
 	char *name;
 	void (*f)(char **args);
 } built_in;
+
+/*Global variable*/
 extern char **environ;
+
+/*Simple shell output prompt*/
 void sh_loop(void);
+
+/*String token helper functions and it customized form*/
 char *eval_str(char *str, char **nxt_ptr, const char *delim);
 char *check_str(char *str, const char *delim);
 char *_strtok(char *str, const char *delim);
 
+/*Custom functions of builtin functions*/
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 char *_getenv(char *name);
 int _unsetenv(char *name);
@@ -35,21 +42,18 @@ char *setenv_cat(char *name, char *value);
 int _setenv(char *name, char *value);
 char **get_args(char *line);
 
+/*Custom string functions*/
 int _strlen(char *s);
 char *_strcat(char *src, char *dest);
 char *_strdup(char *str);
-<<<<<<< HEAD
-int _strcmp(char *str1, char *str2);
-int built_in(char **args);
-=======
 int _strcmp(char *strcmp1, char *strcmp2);
 
+/*Custom functions of builtin commands*/
 int builtin_args(char **args);
 void bui_exit(char **args);
 void bui_env(char **args);
 void bui_unsetenv(__attribute__((unused))char **args);
 void bui_setenv(char **args);
->>>>>>> 210119bfbe8624e2355d7500278e9f000f77ce07
 int execute_cmd(char **args);
 int check_execute(char **args);
 int check_dir(char **args);
